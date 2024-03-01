d51be9d33ad2d2cbe15652585511d270f57b7f5d
Ethan Kim <ehk003@ucsd.edu>
2024-02-29 21:51:46 -0800

    add Maps class

---
+++ _Models/Maps.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace MyGame;
+
+public class Map
+{
+    private readonly Point _mapTileSize = new(4, 3);
+    private readonly Sprite[,] _tiles;
+    public Point TileSize { get; private set; }
+    public Point MapSize { get; private set; }
+
+    public Map()
+    {
+        _tiles = new Sprite[_mapTileSize.X, _mapTileSize.Y];
+
+        Texture2D texture = Globals.Content.Load<Texture2D>("grass");
+
+        TileSize = new(texture.Width, texture.Height);
+        MapSize = new(TileSize.X * _mapTileSize.X, TileSize.Y * _mapTileSize.Y);
+
+        for (int y = 0; y < _mapTileSize.Y; y++)
+        {
+            for (int x = 0; x < _mapTileSize.X; x++)
+            {
+                _tiles[x, y] = new(texture, new(x * TileSize.X, y * TileSize.Y));
+            }
+        }
+    }
+
+    public void Draw()
+    {
+        for (int y = 0; y < _mapTileSize.Y; y++)
+        {
+            for (int x = 0; x < _mapTileSize.X; x++)
+            {
+                _tiles[x, y].Draw();
+            }
+        }
+    }
+}
