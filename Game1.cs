da3e7fdba4571a00a5186dac3a49d91047e53558
Ethan Kim <ehk003@ucsd.edu>
2024-03-01 21:46:57 -0800

    remove redundant using directives

--- Game1.cs
+++ Game1.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-namespace MyGame;
+﻿namespace MyGame;
 
 public class Game1 : Game
 {
