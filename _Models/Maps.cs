261d12a6906f8615646ff84a0c36196e0033945e
Ethan Kim <ehk003@ucsd.edu>
2024-03-07 15:39:31 -0800

    change grass texture

--- _Models/Maps.cs
+++ _Models/Maps.cs
@@ -11,7 +11,7 @@ public class Map
     {
         _tiles = new Sprite[_mapTileSize.X, _mapTileSize.Y];
 
-        Texture2D texture = Globals.Content.Load<Texture2D>("grass");
+        Texture2D texture = Globals.Content.Load<Texture2D>("grass1_upscaled");
 
         TileSize = new(texture.Width, texture.Height);
         MapSize = new(TileSize.X * _mapTileSize.X, TileSize.Y * _mapTileSize.Y);
